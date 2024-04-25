5bff220bf030edc6affd59bb33118e1dd4b70cf0
ht gong <gonghaotong@outlook.com>
2024-04-25 23:52:52 +0800

    create prototype

--- web/src/views/HomeView.vue
+++ web/src/views/HomeView.vue
@@ -1,18 +1,43 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container mt-5">
+      <div class="row">
+        <div class="col-md-6">
+          <h1>Welcome to Pet Services</h1>
+          <p>Experience the best pet care services for your beloved companions.</p>
+        </div>
+        <div class="col-md-6">
+          <!-- Add an image or any other content related to pets here -->
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
   }
 }
 </script>
+
+<style scoped>
+/* Add any custom styles specific to the home page here */
+.home {
+  background-color: #F9F9F9;
+  padding: 50px;
+}
+
+.title {
+  font-size: 36px;
+  font-weight: bold;
+  color: #333;
+}
+
+.description {
+  font-size: 18px;
+  color: #666;
+  margin-top: 20px;
+}
+</style>
